7e9ab251e0c1571dda7ca43b0904cd42b94fba9a
Eduardo Moraton <moraton.eduardo@gmail.com>
2023-11-19 14:41:43 +0100

    Finished: Mai

--- src/utils/mailService.ts
+++ src/utils/mailService.ts
@@ -11,14 +11,28 @@ export async function sendReport(to: string, subject: string, content: Appliance
     await sendEmail({
         to: to,
         subject: subject,
-        html: generateHtmlEmail(content)
+        html: generateHtmlEmail(content, 'This is your weekly report!')
+    } )
+}
+
+export async function sendDalyRoutine(to: string, subject: string, content: ApplianceEntry[]) {
+    await sendEmail({
+        to: to,
+        subject: subject,
+        html: generateHtmlEmail(content, 'This is your Daily report!')
+    } )
+}
+
+export async function sendBestDevice(to: string, subject: string, content: ApplianceEntry[]) {
+    await sendEmail({
+        to: to,
+        subject: subject,
+        html: generateHtmlCheapest(content, "Today: ")
     } )
 }
 
 
-
-  
-function generateHtmlEmail(applianceData: ApplianceEntry[]): string {
+function generateHtmlCheapest(applianceData: ApplianceEntry[], title:string): string {
     const logoUrl = 'https://media.discordapp.net/attachments/1175399192718475334/1175783939273658521/blurred-rainforest-background-free-photo.png?ex=656c7d20&is=655a0820&hm=84fb10288f777861a2e9c5bad2dd07d886f4333a727300030de08a7773df09e4&=&width=1410&height=480';
   
     const htmlContent = `
@@ -50,7 +64,50 @@ function generateHtmlEmail(applianceData: ApplianceEntry[]): string {
         </head>
         <body>
           <img src="${logoUrl}" alt="Company Logo">
-          <h1>This is your weekly report!</h1>  
+          <h1>${title}</h1>  
+          <p>Total Consumption: ${calculateTotalConsumption(applianceData)} kWh</p>
+          <h2>${findCheapestHour(applianceData)}</h2>
+        </body>
+      </html>
+    `;
+  
+    return htmlContent;
+  }
+
+  
+function generateHtmlEmail(applianceData: ApplianceEntry[], title:string): string {
+    const logoUrl = 'https://media.discordapp.net/attachments/1175399192718475334/1175783939273658521/blurred-rainforest-background-free-photo.png?ex=656c7d20&is=655a0820&hm=84fb10288f777861a2e9c5bad2dd07d886f4333a727300030de08a7773df09e4&=&width=1410&height=480';
+  
+    const htmlContent = `
+      <html>
+        <head>
+          <style>
+            h1 {
+              color: #333;
+            }
+            table {
+              border-collapse: collapse;
+              width: 100%;
+            }
+            th, td {
+              border: 1px solid #ddd;
+              padding: 8px;
+              text-align: left;
+            }
+            th {
+              background-color: #f2f2f2;
+            }
+            img {
+              width: 700px; 
+              height: auto; /* Maintain aspect ratio */
+              display: block;
+              margin: 0 auto; /* Center the image */
+            }
+          </style>
+        </head>
+        <body>
+          <img src="${logoUrl}" alt="Company Logo">
+          <h1>${title}</h1>  
           <p>Total Consumption: ${calculateTotalConsumption(applianceData)} kWh</p>
           <table>
             <tr>
@@ -68,7 +125,19 @@ function generateHtmlEmail(applianceData: ApplianceEntry[]): string {
   }
   
   function calculateTotalConsumption(applianceData: ApplianceEntry[]): number {
-    return applianceData.reduce((total, entry) => total + entry.consumption_kwh, 0).toFixed(2);
+    return Number(applianceData.reduce((total, entry) => total + entry.consumption_kwh, 0).toFixed(2));
+  }
+
+  function findCheapestHour(applianceData: ApplianceEntry[]): string {
+    if (applianceData.length === 0) {
+      return "No data available";
+    }
+  
+    let cheapestEntry = applianceData.reduce((cheapest, entry) => {
+      return entry.consumption_kwh < cheapest.consumption_kwh ? entry : cheapest;
+    });
+  
+    return `The cheapest device is: ${cheapestEntry.appliance} with ${cheapestEntry.consumption_kwh.toFixed(2)} kWh`;
   }
   
   function generateTableRows(applianceData: ApplianceEntry[]): string {
